6e4fae1317e20ee28098cb932452a585a2f1f03f
João Victor <44348486+joaoviclps123@users.noreply.github.com>
2021-08-23 10:32:19 -0300

    Add files via upload

--- NAC4.ino
+++ NAC4.ino
@@ -1,9 +1,6 @@
-#include <Adafruit_SSD1306.h>
-#define OLED_Address 0x3C 
-Adafruit_SSD1306 oled(128,64); 
 #include <ArduinoJson.h>
 int x=0;
-int cont = 0;
+volatile unsigned long cont = 0; //número volátil
 int botao;
 int Lx=0;
 int Ly=60;
@@ -21,52 +18,38 @@ float ResLumin;
  
 void setup() {
   Serial.begin(9600);
-  pinMode(2,INPUT);
+  pinMode(2,INPUT_PULLUP);
   pinMode(TermistorPin, INPUT);
   pinMode(A2, INPUT);
-  oled.begin(SSD1306_SWITCHCAPVCC, OLED_Address);
-  oled.clearDisplay();
-  oled.setTextSize(1);
-  oled.setTextColor(WHITE);
+ //-----------  Configuração da Interrução ------------------- //                                 
+
+  attachInterrupt(digitalPinToInterrupt(2),interrupcaoPino2,RISING);  //  Configura o pino2 como interrupção externa do tipo Rising (borda de LOW para HIGH)
 }
 void loop() {
 	StaticJsonDocument<100> json;
 	v = analogRead(A3);
-  	R2 = R1 * (1023.0 / (float)v - 1.0);
-  	logR2 = log(R2);
-  	T = (1.0 / (c1 + c2*logR2 + c3*logR2*logR2*logR2));
-  	Tc = T - 273.15;
-	 ValLumin =  analogRead(A2);
-	 Alpha = 1 / 0.858;
-	 ResLumin = 10000 / ((1023 / ValLumin) - 1); 
-	 Resist = 127410/ResLumin;
-	 Lumin = pow(Resist, Alpha);
-
-	botao = digitalRead(2);
-  	if(botao == LOW){
-		cont = cont + 1;
-		json["Luminosidade"] = Lumin;
-		json["Temperatura"] = Tc;
-		serializeJson(json, Serial);
-		Serial.println();
-		delay(500);
-		if(cont == 2){
-			cont = 0;}
-	 }
-	if(cont == 0){
-		oled.clearDisplay();
-  		oled.setCursor(0,25);
-  		oled.setTextColor(WHITE);
-  		oled.print("Temperatura:");
- 		oled.print(Tc);
-  		oled.display();
+	R2 = R1 * (1023.0 / (float)v - 1.0);
+	logR2 = log(R2);
+	T = (1.0 / (c1 + c2*logR2 + c3*logR2*logR2*logR2));
+	Tc = T - 273.15;
+	ValLumin =  analogRead(A2);
+	Alpha = 1 / 0.858;
+	ResLumin = 10000 / ((1023 / ValLumin) - 1); 
+	Resist = 127410/ResLumin;
+	Lumin = pow(Resist, Alpha);
+	
+	if (millis() - previousMillis >= intervalo){
+	previousMillis = millis();
+	cont = 0;
 	}
-	if(cont == 1){
-		oled.clearDisplay();
-		oled.setCursor(0,25);
-		oled.setTextColor(WHITE);
-		oled.print("Luz:");
-		oled.print(Lumin);
-		oled.display();
-	}
+	
+	
+	json["Luminosidade"] = Lumin;
+	json["Temperatura"] = Tc;
+	serializeJson(json, Serial);
+	Serial.println();
+	delay(7000);
+}
+void interrupcaoPino2(){
+	cont = cont + 1;
 }
